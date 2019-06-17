a8af920a35bcacedad23ac55cf6bf69dda5d6f92
Miko <MikoSecSoS@gmail.com>
2019-06-18 01:09:33 +0800

    update

--- bingSpider.py
+++ bingSpider.py
@@ -29,6 +29,7 @@ class BingSpider:
             "alexatoolbar-alx_ns_ph": "AlexaToolbar/alx-4.0.3",
             "cache-control": "max-age=0",
             "upgrade-insecure-requests": "1",
+            "cookie": "DUP=Q=axt7L5GANVktBKOinLxGuw2&T=361645079&A=2&IG=8C06CAB921F44B4E8AFF611F53B03799; _EDGE_V=1; MUID=0E843E808BEA618D13AC33FD8A716092; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=CADDA53D4AD041148FEB9D0BF646063A&dmnchg=1; MUIDB=0E843E808BEA618D13AC33FD8A716092; ISSW=1; ENSEARCH=BENVER=1; SerpPWA=reg=1; _EDGE_S=mkt=zh-cn&ui=zh-cn&SID=252EBA59AC756D480F67B727AD5B6C22; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; SRCHUSR=DOB=20190616&T=1560789192000; _FP=hta=on; BPF=X=1; SRCHHPGUSR=CW=1341&CH=293&DPR=1&UTC=480&WTS=63696385992; ipv6=hit=1560792905533&t=4; _SS=SID=252EBA59AC756D480F67B727AD5B6C22&HV=1560790599",
             "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
         }
         filename = word + " " + now_time + ".txt"
@@ -49,7 +50,7 @@ class BingSpider:
 
     def main(self):
         pool = Pool()
-        pool.map(self.getUrls, [0] + [8+i*10 for i in range(100)])
+        pool.map(self.getUrls, [i*10 for i in range(100)])
 
 
 if __name__ == "__main__":
